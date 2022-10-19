716d16482fe85b2e3b97462a371f7684bfda3f63
이은찬 <921c@naver.com>
2022-10-19 15:58:44 +0900

    1019

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   //<App /> : 실질적 태그 App.js와 연결됨
   <React.StrictMode> 
     <App />
-  </React.StrictMode>
+  </React.StrictMode>  
 );
 
 // If you want to start measuring performance in your app, pass a function
